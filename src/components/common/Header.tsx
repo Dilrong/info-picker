8fd7ae11924898c30aad85b041866038e75d0112
hslee <hslee@com2us.com>
2025-03-06 10:13:01 +0900

    feat: analytics

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { Card } from "@/components/ui/card";
+import { Analytics } from "@vercel/analytics/react";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <Card className="w-full sticky top-0 z-50 bg-white dark:bg-gray-950 border-b border-gray-200 dark:border-gray-800 shadow-sm">
-      <div className="container mx-auto max-w-5xl flex justify-between items-center px-4">
-        <Link href="/" className="flex items-center space-x-2 group">
-          <span className="text-xl font-bold text-gray-900 dark:text-white group-hover:text-blue-500 transition-colors duration-200">
-            Info Picker
-          </span>
-        </Link>
-      </div>
-    </Card>
+    <>
+      <Analytics />
+      <Card className="w-full sticky top-0 z-50 bg-white dark:bg-gray-950 border-b border-gray-200 dark:border-gray-800 shadow-sm">
+        <div className="container mx-auto max-w-5xl flex justify-between items-center px-4">
+          <Link href="/" className="flex items-center space-x-2 group">
+            <span className="text-xl font-bold text-gray-900 dark:text-white group-hover:text-blue-500 transition-colors duration-200">
+              Info Picker
+            </span>
+          </Link>
+        </div>
+      </Card>
+    </>
   );
 }
